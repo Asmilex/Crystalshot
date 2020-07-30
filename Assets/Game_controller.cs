c093719d864880aebcef37c38ce5d5659579c1bd
Andres <Millan>
2020-07-31 00:57:02 +0200

    Añado controlador de juego

---
+++ Assets/Game_controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class Game_controller : MonoBehaviour
+{
+
+
+    List<(int, int)> jugadores = new List<(int, int)>(); // Stores (instanceID, health)
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void update_health((int, int) player_health) {
+        for (int i = 0; i < jugadores.Capacity; i++) {
+            if (player_health.Item1 == jugadores[i].Item1) {
+                jugadores[i] = player_health;
+            }
+        }
+    }
+}
