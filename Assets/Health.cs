e39da492df1b902a09b63c9d52ec663e015f8911
Juan Ocaña <juanov98@gmail.com>
2020-07-31 21:21:28 +0200

    Las pruebas de UI

---
+++ Assets/Health.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health = 3;
+    public int maxHealth = 3;
+
+    public Image[] hearts;
+    public Sprite fullLife;
+    public Sprite emptyLife;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        if(health > maxHealth) {
+            health = maxHealth;
+        }
+
+        for(int i = 0; i < hearts.Length; ++i) {
+            hearts[i].sprite = (i < health) ? fullLife : emptyLife;
+            hearts[i].enabled = (i < maxHealth) ? true : false; 
+        }
+    }
+}
